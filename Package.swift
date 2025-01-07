8561a55378d8f10e9584075ebb62674ff6a341e1
halkbank-mimari <123956253+halkbank-mimari@users.noreply.github.com>
2025-01-07 18:25:37 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
       targets: ["MLKitFaceDetection", "MLImage", "MLKitVision", "Common"]),
   ],
   dependencies: [
+    .package(url: "https://github.com/google/promises.git", exact: "2.4.0"),
     .package(url: "https://github.com/google/GoogleDataTransport.git", exact: "9.4.0"),
     .package(url: "https://github.com/google/GoogleUtilities.git", exact: "7.13.3"),
     .package(url: "https://github.com/google/gtm-session-fetcher.git", exact: "3.4.1"),
@@ -96,6 +97,7 @@ let package = Package(
         .product(name: "GTMSessionFetcher", package: "gtm-session-fetcher"),
         .product(name: "GoogleDataTransport", package: "GoogleDataTransport"),
         .product(name: "nanopb", package: "nanopb"),
+        .product(name: "FBLPromises", package: "promises"),
       ]),
   ]
 )
